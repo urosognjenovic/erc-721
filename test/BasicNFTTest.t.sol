c8b7b7bfb02f7a3e94147aabbc44d3d972acb447
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-04-21 23:29:39 +0200

    Add one more test

--- test/BasicNFTTest.t.sol
+++ test/BasicNFTTest.t.sol
@@ -9,6 +9,10 @@ import {BasicNFT} from "../src/BasicNFT.sol";
 contract BasicNFTTest is Test {
     DeployBasicNFT private s_deployer;
     BasicNFT private s_basicNFT;
+    address private immutable i_alice = makeAddr("Alice");
+    address private immutable i_bob = makeAddr("Bob");
+    string private constant TOKEN_URI = "";
+    uint256 private constant ALICE_BALANCE_AFTER_MINT = 1;
 
     function setUp() external {
         s_deployer = new DeployBasicNFT();
@@ -18,5 +22,13 @@ contract BasicNFTTest is Test {
     function testNameAndSymbol() external view {
         assertEq(s_basicNFT.name(), NAME);
         assertEq(s_basicNFT.symbol(), SYMBOL);
+    }
+
+    function testMintAndBalance() external {
+        vm.prank(i_alice);
+        s_basicNFT.mintNFT(TOKEN_URI);
+        assertEq(s_basicNFT.balanceOf(i_alice), ALICE_BALANCE_AFTER_MINT);
+        string memory tokenURI = s_basicNFT.tokenURI(ALICE_BALANCE_AFTER_MINT - 1);
+        assertEq(tokenURI, TOKEN_URI);
     }
 }
