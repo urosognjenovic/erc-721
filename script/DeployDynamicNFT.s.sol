99171873fb90b19ea6baa73864942f71137aca55
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-04-24 10:35:03 +0200

    Create EncodeAndDeployDynamicNFT script and test. Fix FIRST_SVG and SECOND_SVG encoding

--- script/DeployDynamicNFT.s.sol
+++ script/DeployDynamicNFT.s.sol
@@ -4,11 +4,12 @@ pragma solidity 0.8.28;
 
 import {Script} from "forge-std/Script.sol";
 import {DynamicNFT} from "../src/DynamicNFT.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 string constant NAME = "DynamicNFT";
 string constant SYMBOL = "DNFT";
-string constant FIRST_SVG = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODAwcHgiIGhlaWdodD0iODAwcHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGQ9Ik0xMiAxMFYzTTEyIDNMOSA2TTEyIDNMMTUgNk02IDEyTDUgMTFNMTggMTJMMTkgMTFNMyAxOEgyMU01IDIxSDE5TTcgMThDNyAxNS4yMzg2IDkuMjM4NTggMTMgMTIgMTNDMTQuNzYxNCAxMyAxNyAxNS4yMzg2IDE3IDE4IiBzdHJva2U9IiMwMDAwMDAiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+DQo8L3N2Zz4=";
-string constant SECOND_SVG = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODAwcHgiIGhlaWdodD0iODAwcHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4NCjxwYXRoIGQ9Ik02IDEyTDUgMTFNMTggMTJMMTkgMTFNMyAxOEgyMU01IDIxSDE5TTcgMThDNyAxNS4yMzg2IDkuMjM4NTggMTMgMTIgMTNDMTQuNzYxNCAxMyAxNyAxNS4yMzg2IDE3IDE4TTEyIDNWMTBNMTIgMTBMMTUgN00xMiAxMEw5IDciIHN0cm9rZT0iIzAwMDAwMCIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiLz4NCjwvc3ZnPg==";
+string constant FIRST_SVG = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODAwcHgiIGhlaWdodD0iODAwcHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNMTIgMTBWM00xMiAzTDkgNk0xMiAzTDE1IDZNNiAxMkw1IDExTTE4IDEyTDE5IDExTTMgMThIMjFNNSAyMUgxOU03IDE4QzcgMTUuMjM4NiA5LjIzODU4IDEzIDEyIDEzQzE0Ljc2MTQgMTMgMTcgMTUuMjM4NiAxNyAxOCIgc3Ryb2tlPSIjMDAwMDAwIiBzdHJva2Utd2lkdGg9IjIiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIvPjwvc3ZnPg==";
+string constant SECOND_SVG = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iODAwcHgiIGhlaWdodD0iODAwcHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNNiAxMkw1IDExTTE4IDEyTDE5IDExTTMgMThIMjFNNSAyMUgxOU03IDE4QzcgMTUuMjM4NiA5LjIzODU4IDEzIDEyIDEzQzE0Ljc2MTQgMTMgMTcgMTUuMjM4NiAxNyAxOE0xMiAzVjEwTTEyIDEwTDE1IDdNMTIgMTBMOSA3IiBzdHJva2U9IiMwMDAwMDAiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIi8+PC9zdmc+";
 
 contract DeployDynamicNFT is Script {
     DynamicNFT private s_dynamicNFT;
@@ -18,5 +19,20 @@ contract DeployDynamicNFT is Script {
         s_dynamicNFT = new DynamicNFT(NAME, SYMBOL, FIRST_SVG, SECOND_SVG);
         vm.stopBroadcast();
         return s_dynamicNFT;
+    }
+}
+
+contract EncodeAndDeployDynamicNFT is Script {
+    string constant BASE_URI = "data:image/svg+xml;base64,";
+
+    function run() external returns (DynamicNFT) {}
+
+    function convertSVGToImageURI(
+        string memory svg
+    ) public pure returns (string memory) {
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(svg)
+        );
+        return string.concat(BASE_URI, svgBase64Encoded);
     }
 }
--- test/DynamicNFTTest.t.sol
+++ test/DynamicNFTTest.t.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.28;
 
 import {Test, console} from "forge-std/Test.sol";
-import {DeployDynamicNFT, NAME, SYMBOL} from "../script/DeployDynamicNFT.s.sol";
+import {DeployDynamicNFT, EncodeAndDeployDynamicNFT, NAME, SYMBOL, FIRST_SVG} from "../script/DeployDynamicNFT.s.sol";
 import {DynamicNFT} from "../src/DynamicNFT.sol";
 
 contract DynamicNFTTest is Test {
@@ -21,5 +21,19 @@ contract DynamicNFTTest is Test {
         vm.prank(i_alice);
         s_dynamicNFT.mintNFT();
         console.log(s_dynamicNFT.tokenURI(0));
+    }
+}
+
+contract EncodeAndDeployDynamicNFTTest is Test {
+    EncodeAndDeployDynamicNFT private s_deployer;
+    string private constant RAW_SVG = '<svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 10V3M12 3L9 6M12 3L15 6M6 12L5 11M18 12L19 11M3 18H21M5 21H19M7 18C7 15.2386 9.23858 13 12 13C14.7614 13 17 15.2386 17 18" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+
+    function setUp() external {
+        s_deployer = new EncodeAndDeployDynamicNFT();
+    }
+
+    function testConvertSVGToImageURI() external view {
+        string memory encodedSVG = s_deployer.convertSVGToImageURI(RAW_SVG);
+        assertEq(encodedSVG, FIRST_SVG);
     }
 }
